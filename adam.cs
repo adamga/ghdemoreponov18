a65adda4ef9c79d64f15e903d917afde192d4f00
adamga <adamga@microsoft.com>
2025-06-05 08:46:25 -0400

    feat: Remove welcome messages from Greetings class

--- adam.cs
+++ adam.cs
@@ -3,14 +3,9 @@ using System;
 public class Greetings
 {
 
-    /// <summary>
-    /// The entry point of the application.
-    /// </summary>
-    /// <param name="args">An array of command-line arguments.</param>
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome to C# programming.");
-        Console.WriteLine("Let's explore the features of C# 13.0 together.");
+
     }
 }
